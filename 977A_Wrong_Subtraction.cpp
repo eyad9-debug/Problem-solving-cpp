55b9b4b7a7efe03e137e2a7812daad9c6468eda9
eyad9-debug <eyadabdelaleem9@gmail.com>
2026-07-29 11:14:41 +0300

    Create 977A_Wrong_Subtraction.cpp

---
+++ 977A_Wrong_Subtraction.cpp
@@ -0,0 +1,27 @@
+/*
+* Problem: Codeforces 977A Wrong Subtraction
+* Time Complexity: O(N)
+* Space Complexity: O(1)
+* Approach: Using the algorithm mentioned in the problem
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n,k;
+    cin>> n >>k;
+
+    for(int i=0; i<k; i++){
+        if(n%10==0){
+            n/=10;
+        }
+        else{
+            n--;
+        }
+    }
+    cout << n << "\n";
+    return 0;
+}
