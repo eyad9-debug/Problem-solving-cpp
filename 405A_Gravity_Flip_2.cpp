d827361b87c4558b9471f8212206e02629bdec19
eyad9-debug <eyadabdelaleem9@gmail.com>
2026-08-12 13:04:20 +0300

    Create 405A_Gravity_Flip_2.cpp

---
+++ 405A_Gravity_Flip_2.cpp
@@ -0,0 +1,71 @@
+/*
+* Problem: Codeforces 405A Gravity Flip
+* Time Complexity: O(N log N)
+* Space Complexity: O(N)
+* Approach: Divide, Conquer and Combine
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void merge(vector<int>& arr, int left, int mid, int right){
+    int n1= mid-left+1;
+    int n2= right-mid;
+
+    vector<int> L(n1);
+    vector<int> R(n2);
+
+    for(int i=0; i<n1; i++) L[i]=arr[left+i];
+    for(int i=0; i<n2; i++) R[i]=arr[mid+1+i];
+
+    int i=0, j=0, k=left;
+
+    while(i<n1 && j<n2){
+        if(L[i] <= R[j]){
+            arr[k]=L[i];
+            i++;
+        }
+        else{
+            arr[k]=R[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<n1){
+        arr[k]=L[i];
+        i++;
+        k++;
+    }
+    while(j<n2){
+        arr[k]=R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergesort(vector<int>& arr, int left, int right){
+    if(left>=right) return;
+    int mid= left+ (right-left)/2;
+
+    mergesort(arr, left, mid);
+    mergesort(arr, mid+1, right);
+
+    merge(arr, left, mid, right);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>> arr[i];
+    }
+
+    mergesort(arr, 0, n-1);
+
+    for(int i=0; i<n; i++){
+        cout<< arr[i]<< " ";
+    }
+    return 0;
+}
